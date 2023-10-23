37cf7df73443176444df595b4f1bea3816242113
mufaqar <mufaqar@gmail.com>
2023-10-23 20:16:20 +0500

    SEo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,33 @@ import Chose from '../components/chose/chose'
 import Pic from '../components/picss/pic'
 import Versatile from '../components/versatile/versatile'
 import Footer from '../components/footer/footer'
+import Head from 'next/head'
 export default function Home() {
   return (
-  <main>
+
+    <>
+    <Head>
+        <title>Truckala - Transportation & Logistics</title>
+        <meta name="description" content="Get all-in-one Platform for Transportation & Logistics.Streamline, Optimize, Excel with Truckala" />
+        <link rel="canonical" href="https://www.truckala.com" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Home" />
+        <meta property="og:description" content="Get all-in-one Platform for Transportation & Logistics.Streamline, Optimize, Excel with Truckala" />
+        <meta property="og:url" content="https://www.truckala.com" />
+        <meta property="og:site_name" content="Get all-in-one Platform for Transportation & Logistics.Streamline, Optimize, Excel with Truckala" />
+        <meta property="article:publisher" content="https://www.facebook.com/cablemovers" />
+        <meta property="article:modified_time" content="2023-07-06T22:58:46+00:00" />
+        <meta property="og:image" content="" />
+        <meta property="og:image:width" content="700" />
+        <meta property="og:image:height" content="467" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@truckala" />
+        <meta name="twitter:label1" content="Est. reading time" />
+        <meta name="twitter:data1" content="07 minutes" />
+      </Head>
+      <main>
   <Main/>
   <Blog/>
   <Versatile/>
@@ -16,5 +40,7 @@ export default function Home() {
   <Pic/>
  <Footer/> 
   </main>
+    </>
+  
   )
 }

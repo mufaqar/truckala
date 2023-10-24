07764f5ec90256b17d6d897b908935668263c1a0
mufaqar <mufaqar@gmail.com>
2023-10-24 18:50:23 +0500

    favicon

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -19,13 +19,13 @@ function Main() {
           <div className="flex flex-col sm:flex-row gap-10 justify-between py-10 items-center ">
             <div className="text-white md:w-2/5 w-full">
               <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-semibold mb-2">
-                Get all-in-one Platform for
+              Get all-in-one Platform for 
               </h2>
               <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-[78px] font-bold leading-snug mb-4">
                 Transportation & Logistics.
               </h1>
               <p className="text-base sm:text-lg md:text-xl lg:text-2xl py-3">
-                Streamline, Optimize, Excel with Truckala.
+              Streamline, Optimize, Excel with Truckala. 
               </p>
               <div className="mt-5">
                 <Link href="#" className="text-white border border-white rounded-2xl px-10 py-3 text-[21px] leading-[42px]">

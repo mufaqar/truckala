f3e8437b8f625eccd4f80acb9831e6a3e2403d04
mufaqar <mufaqar@gmail.com>
2023-10-24 19:47:08 +0500

    update

--- src/app/load-market/page.tsx
+++ src/app/load-market/page.tsx
@@ -1,6 +1,7 @@
 import Main from '@/components/main/main'
 import Footer from '@/components/footer/footer'
 import React from 'react'
+import { IoIosArrowDropdownCircle } from 'react-icons/io'
 
 function LoadMarket() {
     return (
@@ -17,12 +18,13 @@ function LoadMarket() {
                     <p>   At Load Market, we're all about connecting, optimizing, and expanding your transportation business. With us, you can increase your revenue, achieve operational efficiency, and minimize empty backloads. We're excited to have you on board as we revolutionize the world of transportation services together. Welcome to a new era of possibilities!"
                     </p>
 
-                    <h2> Our platform is designed to help you thrive;</h2>
-
-                    <p>    Connect and Grow: Linking Transportation Providers to Shippers.   </p>
-                    <p>   Efficiency at Its Best: Get Roadside Assistance.   </p>
-                    <p>    Reduce Empty Backloads: Optimize Routes and Maximize Your Business Potential.   </p>
-                    <p>    Join and revolutionize the transportation ecosystem today!   </p>
+                    <h2 className='text-3xl font-semibold '> Our platform is designed to help you thrive;</h2>
+                    <ul className="grid sm:grid-cols-3 grid-cols-2 mt-5 gap-4 w-full">
+                        <li className="flex gap-2 items-center">  <IoIosArrowDropdownCircle className="text-blue-700 text-xl" />   Connect and Grow: Linking Transportation Providers to Shippers.   </li>
+                        <li className="flex gap-2 items-center">  <IoIosArrowDropdownCircle className="text-blue-700 text-xl" />   Efficiency at Its Best: Get Roadside Assistance.   </li>
+                        <li className="flex gap-2 items-center">  <IoIosArrowDropdownCircle className="text-blue-700 text-xl" />    Reduce Empty Backloads: Optimize Routes and Maximize Your Business Potential.   </li>
+                        <li className="flex gap-2 items-center">   <IoIosArrowDropdownCircle className="text-blue-700 text-xl" />   Join and revolutionize the transportation ecosystem today!   </li>
+                    </ul>
 
                 </div>
             </div>

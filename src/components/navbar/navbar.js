11a676cf59a84c316365807d10f3445961f71289
mufaqar <mufaqar@gmail.com>
2023-10-25 19:55:38 +0500

    nav

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -19,8 +19,8 @@ function Navbar() {
           </Link>
         </div>
         <div className="hidden md:flex items-center space-x-8">
-          <Link href="#products" className="text-white hover:text-blue-300">Products</Link>
-          <Link href="#industries" className="text-white hover:text-blue-300">Industries</Link>
+          <Link href="/#products" className="text-white hover:text-blue-300">Products</Link>
+          <Link href="/#industries" className="text-white hover:text-blue-300">Industries</Link>
           <Link href="/load-market" className="text-white hover:text-blue-300">Load Market</Link>
           <Link href="#company" className="text-white hover:text-blue-300">About</Link>
           <Link href="/contact-us" className="text-base font-semibold  bg-white text-[#1D36E4] px-6 py-3 rounded-[3px]">
@@ -37,8 +37,8 @@ function Navbar() {
       </div>
       {isOpen && (
         <div className="md:hidden w-full absolute top-28 left-0 right-0 bg-[#1D36E4] py-10 px-4">
-          <Link href="#products" className="block text-white py-2 hover:opacity-70">Products</Link>
-          <Link href="#industries" className="block text-white py-2 hover:opacity-70">Industries</Link>
+          <Link href="/#products" className="block text-white py-2 hover:opacity-70">Products</Link>
+          <Link href="/#industries" className="block text-white py-2 hover:opacity-70">Industries</Link>
           <Link href="load-market" className="block text-white py-2 hover:opacity-70">Load Market</Link>
           <Link href="#company" className="block text-white py-2 hover:opacity-70">About</Link>
         </div>

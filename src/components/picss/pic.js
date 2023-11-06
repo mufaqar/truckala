a9817657e2492a4d3d90a6429ad61ebcae2096f5
mufaqar <mufaqar@gmail.com>
2023-11-06 19:09:30 +0500

    Book Demo remove

--- src/components/picss/pic.js
+++ src/components/picss/pic.js
@@ -9,9 +9,9 @@ function Pic() {
           <h2 className='text-2xl md:text-3xl xl:text-4xl font-semibold mb-10'>
             Ready to get started with Truckala
           </h2>
-          <Link href="/contact-us" className="text-black border border-black rounded-2xl px-8 py-2.5 text-xl">
-            Book a demo
-          </Link>
+          {/* <Link href="/contact-us" className="text-black border border-black rounded-2xl px-8 py-2.5 text-xl">
+            Get In Touch
+          </Link> */}
         </div>
       </div>
     </div>
